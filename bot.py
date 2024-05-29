ed6002df9ec362ef797ab4ff567f962b077d81aa
BuildTools <unconfigured@null.spigotmc.org>
2024-05-29 12:53:57 +0600

    fixed sorting data based on date range

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from dotenv import dotenv_values
 from aiogram import Bot, Dispatcher, executor, types
 import pymongo
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import motor.motor_asyncio
 
 config = dotenv_values(".env")
@@ -30,7 +30,7 @@ invalid_input_msg = """
 @dp.message_handler()
 async def start(message: types.Message):
     # Проверка на правильность введённых данных
-    # try:
+    try:
         parsed_json = json.loads(message.text)
         if not isinstance(parsed_json, dict) or [key for key in ["dt_from", "dt_upto", "group_type"] if key not in parsed_json]:
             await bot.send_message(message.chat.id, invalid_input_msg)
@@ -39,8 +39,6 @@ async def start(message: types.Message):
         mongo_client = motor.motor_asyncio.AsyncIOMotorClient(config["MONGO_CLIENT"])
         db = mongo_client["sampleDB"]
         collection = db["sample_collection"]
-        
-        result = {"dataset": [], "labels": []}
 
         dt_from = datetime.strptime(parsed_json["dt_from"], "%Y-%m-%dT%H:%M:%S")
         dt_upto = datetime.strptime(parsed_json["dt_upto"], "%Y-%m-%dT%H:%M:%S")
@@ -54,19 +52,30 @@ async def start(message: types.Message):
             date_format_time = ":00:00"
 
         pipeline = [
-            {"$sort": {"dt": 1}},
             {"$match": {"dt": {"$gte": dt_from, "$lte": dt_upto}}},
-            {"$group": {"_id": {"$dateToString": {"format": date_format, "date": "$dt"}}, "total_value": {"$sum": "$value"}}}
+            {"$group": {"_id": {"$dateToString": {"format": date_format, "date": "$dt"}}, "total_value": {"$sum": {"$cond": {"if": {"$eq": ["$value", 0]}, "then": 0, "else": "$value"}}}}}
         ]
-        
-        async for document in collection.aggregate(pipeline):
-            result["dataset"].append(document.get('total_value'))
-            result["labels"].append(document.get('_id') + date_format_time)
 
-        await bot.send_message(message.chat.id, result)
+        cursor = collection.aggregate(pipeline)
+        documents = await cursor.to_list(length=None)
+
+        current_date = dt_from
+        while current_date <= dt_upto:
+            date = datetime.strftime(current_date, date_format)
+            if (date not in [d.get("_id") for d in documents]):
+                 documents.append({"_id": date, "total_value": 0})
+            current_date += timedelta(days=1)
         
-    # except:
-    #     await bot.send_message(message.chat.id, invalid_input_msg)
+        result = {"dataset": [], "labels": []}
+
+        for document in sorted(documents, key=lambda x: x['_id']):
+            result["dataset"].append(document['total_value'])
+            result["labels"].append(document['_id'] + date_format_time)
+
+        await bot.send_message(message.chat.id, str(result).replace("\'", "\""))
+        
+    except:
+        await bot.send_message(message.chat.id, invalid_input_msg)
 
 
 if __name__ == "__main__":
